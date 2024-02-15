7ef2aecc0671777e98798a80e0f428533d047fe9
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-15 12:57:32 -0600

    Decimoquinto ejercicio

--- tsemana2/Program.cs
+++ tsemana2/Program.cs
@@ -1,15 +1,9 @@
-﻿//Escribe un programa que lea una cadena de texto y busque una palabra en ella
+﻿//Escribe un programa que lea una cadena de texto y cuente el número de vocales que contiene.
+
+using System.Text.RegularExpressions;
 
 Console.WriteLine("Escriba el texto");
 string texto = Console.ReadLine();
-Console.WriteLine("Cual es la palabra que busca?");
-string palabra = Console.ReadLine();
-if (texto.Contains(palabra))
-
-
-    Console.WriteLine("La palabra si está en el texto");
-
-else
-
-    Console.WriteLine("La palabra no esta en el texto");
+int vocales = Regex.Matches(texto,"[aeiouAEIOU]").Count;
+Console.WriteLine("Su palabra tiene: "+vocales);
 
