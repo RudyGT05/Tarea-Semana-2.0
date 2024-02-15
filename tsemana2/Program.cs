a84031844607d874418369daea227b56a249c076
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-15 11:49:48 -0600

    doceavo ejercicio

--- tsemana2/Program.cs
+++ tsemana2/Program.cs
@@ -1,6 +1,6 @@
-﻿//Escribe un programa que lea una cadena de texto y la imprima en mayúsculas.
+﻿//Escribe un programa que lea una cadena de texto y la imprima en minúsculas
 
 Console.WriteLine("Escriba el texto");
 string texto = Console.ReadLine();
-string txtm = texto.ToUpper();
+string txtm = texto.ToLower();
 Console.WriteLine(txtm);
