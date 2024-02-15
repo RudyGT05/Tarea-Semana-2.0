9dce21a129c696c02db23ee006910dfd30343855
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-15 13:36:04 -0600

    vigesimo ejercicio

--- tsemana2/Program.cs
+++ tsemana2/Program.cs
@@ -1,8 +1,8 @@
-﻿//Escribe un programa que calcule la temperatura en grados Fahrenheit a partir de la temperatura en grados Celsius
+﻿//Escribe un programa que calcule la temperatura en grados Celsius a partir de la temperatura en grados Fahrenheit.
 
-Console.WriteLine("Ingrese la temperatura en grados celsius:");
-double celsius = double.Parse(Console.ReadLine());
-double fahrenheit = (celsius * 9 / 5) + 32;
+Console.WriteLine("Ingrese la temperatura en grados Fahrenheit:");
+double fahrenheit = double.Parse(Console.ReadLine());
+double celsius = (fahrenheit - 32) * 5/9;
 
-Console.WriteLine("la temperatura en grados Fahrenheit es de:" +fahrenheit);
+Console.WriteLine("la temperatura en grados Celsius es de:" + celsius);
 
