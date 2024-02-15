c13b335419fd642c116a9038bb88408fbe15e1df
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-15 11:43:18 -0600

    Decimo Ejercicio

--- tsemana2/Program.cs
+++ tsemana2/Program.cs
@@ -1,11 +1,11 @@
-﻿//división de dos números enteros
+﻿//módulo de dos números enteros
 
 using System.Diagnostics.CodeAnalysis;
 
-int n1 = 10;
+int n1 = 12;
 int n2 = 5;
-float division = n1 / n2;
+float mod = n1 % n2;
 
 
 
-Console.WriteLine("La division de los dos numeros es: "+ division);
+Console.WriteLine("La division de los dos numeros es: "+ mod);
