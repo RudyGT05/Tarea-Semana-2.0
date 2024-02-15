c9ff60dfa82661fadddd29061785b62d5070641b
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-15 12:05:38 -0600

    Decimocuarto Ejercicio

--- tsemana2/Program.cs
+++ tsemana2/Program.cs
@@ -1,6 +1,15 @@
-﻿//Escribe un programa que lea una cadena de texto y la imprima al revés
+﻿//Escribe un programa que lea una cadena de texto y busque una palabra en ella
 
 Console.WriteLine("Escriba el texto");
 string texto = Console.ReadLine();
-string txtm = new string(texto.Reverse().ToArray());
-Console.WriteLine(txtm);
+Console.WriteLine("Cual es la palabra que busca?");
+string palabra = Console.ReadLine();
+if (texto.Contains(palabra))
+
+
+    Console.WriteLine("La palabra si está en el texto");
+
+else
+
+    Console.WriteLine("La palabra no esta en el texto");
+
